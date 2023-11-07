7a847001a0a452541433eeb42c0045619d1558f9
LaCapitainerie <66835496+LaCapitainerie@users.noreply.github.com>
2023-11-07 14:44:36 +0100

    Main commit
    added core files

---
+++ color.py
@@ -0,0 +1,184 @@
+class color():
+    """
+    color((r, g, b))\n
+    color("#000000")
+    """
+    def __init__(self, rgb:tuple|str) -> str:
+        self.value = (rgb if type(rgb) == tuple else tuple(int((rgb.lstrip('#'))[i:i+2], 16) for i in (0, 2, 4)))
+        return
+    
+    def __eq__(self, __value: object) -> bool:
+        return (type(__value) == color) and (self.value == __value.value)
+    
+    def __str__(self) -> str:
+        return ('#{:02x}{:02x}{:02x}'.format(*self.value)).upper()
+    
+    def __repr__(self) -> str:
+        return self.__str__()
+    
+    def __sub__(self, other):
+        if type(other) == color:
+            return color(tuple((a-b) for a,b in list(zip(self.value, other.value))))
+        else:
+            raise ValueError("Unsupported operand type")
+        
+    def __add__(self, other):
+        if type(other) == color:
+            return color(tuple((a+b) for a,b in list(zip(self.value, other.value))))
+        else:
+            raise ValueError("Unsupported operand type")
+
+    def __int__(self) -> int:
+        return (self.value[0] << 16) + (self.value[1] << 8) + self.value[2]
+    
+    def hsv(self) -> tuple:
+        rgb = self.value
+
+        r, g, b = rgb[0] / 255.0, rgb[1] / 255.0, rgb[2] / 255.0
+
+        max_value = max(r, g, b)
+        min_value = min(r, g, b)
+
+        v = max_value * 100
+
+        if max_value != 0:
+            s = ((max_value - min_value) / max_value) * 100
+        else:
+            s = 0
+
+        if max_value == min_value:
+            h = 0
+        else:
+            if max_value == r:
+                h = ((g - b) / (max_value - min_value)) % 6
+            elif max_value == g:
+                h = ((b - r) / (max_value - min_value)) + 2
+            elif max_value == b:
+                h = ((r - g) / (max_value - min_value)) + 4
+            h *= 60
+
+        h = (h+360)%360
+
+        return (int(h), int(s), int(v))
+
+
+def color_moy(*args:color) -> color:
+    def moy(args:tuple) -> int:
+        i = 0
+        som = 0
+        for item in args:
+            som += item
+            i+=1
+        return som//i
+    return color(tuple(map(moy, zip(*map(lambda x: x.value, args)))))
+
+
+def rgb_distance(color1:color, color2:color) -> float:
+    r1, g1, b1 = color1.value
+    r2, g2, b2 = color2.value
+    return ((r1 - r2) ** 2 + (g1 - g2) ** 2 + (b1 - b2) ** 2)**(1/2)
+
+def find_closest_color(tc:color, cl:list[color]):
+    closest_distance = float('inf')
+    for c in cl:
+        distance = rgb_distance(tc, c)
+        if distance < closest_distance:
+            closest_distance = distance
+            closest_color = c
+            sub = tc - c
+
+    return closest_color, sub
+
+def croix(value:color, val:list[color], pal:list[color]) -> color:
+
+    start, sub = find_closest_color(value, val)
+    
+    end = val[(val.index(start) + (-1 if (int(start) > int(value)) else 1))%len(val)]
+
+    percent = int(value - start) * 100 / int(end - start)
+
+
+
+    other_start = int(pal[val.index(start)])
+
+    other_end = int(pal[(val.index(start) + (-1 if (int(start) > int(value)) else 1))%len(pal)])
+
+    final = int(other_end + (percent * (other_start - other_end) / 100))
+
+    return color(hex(final)[2:].upper().zfill(6))
+
+def hsv_to_rgb(h, s, v):
+
+    h = (h % 360) / 360.0
+    s = max(0, min(1, s / 100.0))
+    v = max(0, min(1, v / 100.0))
+
+    if s == 0:
+        r = g = b = int(v * 255)
+    else:
+        h *= 6
+        i = int(h)
+        f = h - i
+        p = int(255 * v * (1 - s))
+        q = int(255 * v * (1 - s * f))
+        t = int(255 * v * (1 - s * (1 - f)))
+
+        if i == 0:
+            r, g, b = int(v * 255), t, p
+        elif i == 1:
+            r, g, b = q, int(v * 255), p
+        elif i == 2:
+            r, g, b = p, int(v * 255), t
+        elif i == 3:
+            r, g, b = p, q, int(v * 255)
+        elif i == 4:
+            r, g, b = t, p, int(v * 255)
+        else:
+            r, g, b = int(v * 255), p, q
+
+    return r, g, b
+
+def getpourcent(form:color, to:color, c:color) -> float:
+    
+    one_ = tuple(map(clamp, form.hsv()))
+    two_ = tuple(map(clamp, to.hsv()))
+    trois = tuple(map(clamp, c.hsv()))
+
+    f = []
+
+    for i in range(3):
+        one = min(one_, two_, key=lambda x: x[i])
+        two = max(one_, two_, key=lambda x: x[i])
+
+        cent = two[i] - one[i]
+        val = trois[i] - one[i]
+
+        pourcent = one[i]/100 if cent == 0 else (val/cent)
+
+        f.append(pourcent)
+
+    return f
+
+def getcolor(form:color, to:color, percent:list[int]) -> color:
+
+    f = []
+    for i in range(3):
+        one_ = tuple(map(clamp, form.hsv()))
+        two_ = tuple(map(clamp, to.hsv()))
+
+        one = min(one_, two_, key=lambda x: x[0])[i]
+        two = max(one_, two_, key=lambda x: x[0])[i]
+
+        value = one + int((percent[i]) * (two - one))
+        
+        f.append(value)
+
+    return color(hsv_to_rgb(*f))
+
+def clamp(n):
+    p = n%5
+    n += 5-p if round(p)>2 else -p
+    return n
+
+def transfo(moule:list[color], objectif:list[color], c:color) -> color:
+    return getcolor(objectif[0], objectif[1], getpourcent(moule[0], moule[1], c))
---
+++ draw.py
@@ -0,0 +1,82 @@
+from PIL import Image;
+from color import *;
+
+
+
+pal1 = list(map(color, [ "#77021D", "#F6B339", "#DA7B27", "#D7572B", "#C23028" ]))
+pal2 = list(map(color, [ "#F88F52", "#FBCE9E", "#7ACFB0", "#00BDC8", "#1C5588" ]))
+pal3 = list(map(color, [ "#ECF39E", "#90A955", "#4F772D", "#31572C", "#132A13" ]))
+pal4 = list(map(color, [ "#598B8E", "#FFFFFF", "#FFDCCB", "#F39440", "#CA5940" ]))
+pal5 = list(map(color, [ "#0D1220", "#1C2A42", "#3C5772", "#6F86A7", "#ECE6CE" ]))
+pal6 = list(map(color, [ "#F0604D", "#F38071", "#F79F95", "#FBBFB8", "#FFDFDB" ]))
+pal7 = list(map(color, [ "#F88F52", "#FBCE9E", "#7ACFB0", "#00BDC8", "#1C5588" ]))
+pal8 = list(map(color, [ "#FE895E", "#F99F72", "#CD6889", "#96527A", "#5B374D" ]))
+pal9 = list(map(color, [ "#5DA5B3", "#E0E2E8", "#B0C0D4", "#4E5772", "#1C1C1F" ]))
+
+sens = 1
+
+
+
+
+
+
+
+
+def draw(image_path:str="", path_result:str="result.png", palette:list[color]|list[str]=[], soft_edges:bool=False) -> str:
+    
+    # Test if the file path is correct else raise error
+    # Then convert the image to rgb and prepare the other image
+    if image_path == "":
+        raise ValueError(f"path does not lead to an image")
+        
+    image = Image.open(image_path)
+    if image.mode != 'RGB':
+        image = image.convert('RGB')
+    newimage = Image.new(mode="RGB", size=image.size, color = (255, 255, 255))
+
+    
+    #list(map(color, [ "#5DA5B3", "#E0E2E8", "#B0C0D4", "#4E5772", "#1C1C1F" ]))
+    # Setting up our constant
+    PAL = ([color("FFFFFF"), color("000000")] + (palette if type(pal9[0]) == color else list(map(color, palette))))     # custom palette
+    VALUES  = ([color("FFFFFF"), color("000000")] + list(map(color, [ "#FF0000", "#FF00FF", "#0000FF", "#00FFFF", "#00FF00" ])))     # beacon of the circle of color
+    WIDTH, HEIGHT = image.size
+
+
+    for x in range(WIDTH):
+        for y in range(HEIGHT):
+            c = color(image.getpixel((x, y)))
+
+            # If the color is a round color put the corresponding value
+            # to avoid make all the calculus
+            if(c in VALUES):
+                newimage.putpixel((x, y), PAL[VALUES.index(c)].value)
+                continue
+
+
+            # To find the corresponding color we take the 2 closest color
+            # Make the complex function corresponding to a line from the first to the second
+            # Then we take the percentage of the line our color in on
+            # We make the complex function for our PALette and get the color at N% 
+            start, sub = find_closest_color(c, VALUES)
+            end = VALUES[(VALUES.index(start) + (-1 if ((c.hsv()[0] > start.hsv()[0])) else 1))%len(VALUES)]
+            final = getcolor(PAL[VALUES.index(start)], PAL[(VALUES.index(start) + (-1 if ((c.hsv()[0] > start.hsv()[0])) else 1))%len(PAL)], getpourcent(start, end, c))
+            newimage.putpixel((x, y), final.value)
+            
+    
+    # Blend edge of the shape
+    if soft_edges:
+        for x in range(1, WIDTH-1):
+            for y in range(1, HEIGHT-1):
+                if(newimage.getpixel((x, y)) == (0, 0, 0)):
+                    newimage.putpixel((x,y), color_moy(*[color(newimage.getpixel((x+i, y+j))) for i in range(-1, 2) for j in range(-1, 2)]).value)
+
+            
+
+    newimage.save(path_result)
+    return path_result
+
+
+
+    
+    
+draw("moule.png", palette=pal9, soft_edges=False)
